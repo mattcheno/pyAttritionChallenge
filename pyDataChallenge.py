90c1feeb1f67b0eb3efbf16bb375ad4b6ac32825
Matt Chenoweth <matthew.william.chenoweth@gmail.com>
2017-08-12 14:53:58 -0700

    Get to a stopping point

--- pyDataChallenge.py
+++ pyDataChallenge.py
@@ -2,26 +2,37 @@
 '''
 
 '''
-print('Hey!')
 
 ''' imports '''
-import pandas as pd
+import pandas as pd #Pandas
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import sys
 from variablez import Variablez
 miVarz = Variablez()
 
-''' import csv '''
+''' read csv '''
 df = pd.read_csv(miVarz.inFileString,
 	dtype='object')
+
+''' convert columns to integers and categories ''' 
+df['Attrition'] = df['Attrition'].map(miVarz.inYesNoBoo)
+df[miVarz.inDFCatCols] = df[miVarz.inDFCatCols].apply(pd.Categorical)
+df[miVarz.inDFIntCols] = df[miVarz.inDFIntCols].apply(pd.to_numeric)
 
 ''' print some stats to check that everything is working '''
 print(df.head(5))
 print(df.describe())
-print(df.columns.to_series().groupby(df.dtypes).groups)
+#print(df.columns.to_series().groupby(df.dtypes).groups)
 
+''' ===== Playing around ===== 
+y = df['Attrition']
+X = df[['MonthlyIncome', 'TotalWorkingYears']]
 
-''' convert columns to integers and categories ''' 
-df[miVarz.inDFCatCols] = df[miVarz.inDFCatCols].apply(pd.astype('category'))
-df[miVarz.inDFIntCols] = df[miVarz.inDFIntCols].apply(pd.to_numeric)
+X = sm.add_constant(X)
+est = sm.OLS(y, X).fit()
 
-print(df.columns.to_series().groupby(df.dtypes).groups)
+print(est.summary())
+'''
--- variablez.py
+++ variablez.py
@@ -7,9 +7,15 @@ class Variablez():
 		Initialize Parameters and Settings
 		'''
 		self.inFileString = 'AnalyticsChallenge1-Train.csv'
-		self.inDFCatCols= ['Attrition',
+		self.inYesNoBoo= {
+		'Yes':1,
+		'No':0
+		}
+		self.inDFCatCols= [
+#		'Attrition',
 		'BusinessTravel',
-		'Department',
+		'Department', 
+		'Education',
 		'EducationField',
 		'EmployeeCount',
 		'EmployeeNumber',
@@ -21,21 +27,27 @@ class Variablez():
 		'JobSatisfaction',
 		'MaritalStatus',
 		'Over18',
-		'OverTime',
-		'PerformanceRating',
-		'RelationshipSatisfaction',
-		'StandardHours',
-		'StockOptionLevel',
-		'WorkLifeBalance']
-		self.inDFIntCols=['Age', 
+		'OverTime'
+		]
+		self.inDFIntCols=[
+		'Age', 
+#		'Attrition',
+		'DailyRate', 
+		'DistanceFromHome', 
 		'HourlyRate', 
 		'MonthlyIncome', 
 		'MonthlyRate', 
 		'NumCompaniesWorked',
 		'PercentSalaryHike', 
+		'PerformanceRating',
+		'RelationshipSatisfaction',
+		'StandardHours',
+		'StockOptionLevel',
 		'TotalWorkingYears', 
 		'TrainingTimesLastYear', 
+		'WorkLifeBalance',
 		'YearsAtCompany', 
 		'YearsInCurrentRole', 
 		'YearsSinceLastPromotion', 
-		'YearsWithCurrManager']
+		'YearsWithCurrManager'
+		]
