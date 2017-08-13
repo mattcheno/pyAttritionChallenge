29fd687fec9510f769039f16d980ed487345e04d
Matt Chenoweth <matthew.william.chenoweth@gmail.com>
2017-08-12 19:58:32 -0700

    Investigate using Dummy Variables
    reference :: http://blog.yhat.com/posts/logistic-regression-python-rodeo.html

--- pyDataChallenge.py
+++ pyDataChallenge.py
@@ -5,11 +5,13 @@
 
 ''' imports '''
 import pandas as pd #Pandas
-import numpy as np
+import numpy as np #Numpy
+import pylab as pl
 import matplotlib
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import sys
+import pprint
 from variablez import Variablez
 miVarz = Variablez()
 
@@ -22,17 +24,27 @@ df['Attrition'] = df['Attrition'].map(miVarz.inYesNoBoo)
 df[miVarz.inDFCatCols] = df[miVarz.inDFCatCols].apply(pd.Categorical)
 df[miVarz.inDFIntCols] = df[miVarz.inDFIntCols].apply(pd.to_numeric)
 
-''' print some stats to check that everything is working '''
+''' print some stats to check that everything is working 
 print(df.head(5))
 print(df.describe())
-#print(df.columns.to_series().groupby(df.dtypes).groups)
+print(df.columns.to_series().groupby(df.dtypes).groups) '''
 
-''' ===== Playing around ===== 
-y = df['Attrition']
-X = df[['MonthlyIncome', 'TotalWorkingYears']]
-
-X = sm.add_constant(X)
-est = sm.OLS(y, X).fit()
-
-print(est.summary())
+''' ===== Trying some dummy variables ===== 
+print(df.columns)
+dummy = pd.get_dummies(df['BusinessTravel'], prefix='Bt')
+print(dummy)
 '''
+
+''' ===== Begin Building Model ===== '''
+data = df[miVarz.inDFIntCols]
+pprint.pprint(miVarz.inDFIntCols)
+
+''' ===== Loop through Categoricals, make dummy variables, and add 
+          to the model                                                ===== '''
+for col in miVarz.inDFCatCols:
+	dummy = pd.get_dummies(df[col], prefix=col)
+	dName = col + '_2'
+	data = data.join(dummy.ix[:, dName:])
+	print(col)
+
+print(data.describe())
